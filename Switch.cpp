8a2fe0767be0bd0c201a4b3ee53adc339cf5ebf5
Adhi Harika <152500458+adhiharika123@users.noreply.github.com>
2024-10-02 16:14:40 +0530

    Add files via upload

---
+++ Switch.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int day;
+    cout<<"Enter a day:";
+    cin>>day;
+    //switch
+    switch(day){
+        case 1:
+            cout<<"MONDAY";
+            break;
+        case 2:
+            cout<<"TUESDAY";
+            break;
+        case 3:
+            cout<<"WEDNESDAY";
+            break;
+        case 4:
+            cout<<"THURSDAY";
+            break;
+        case 5:
+            cout<<"FRIDAY";
+            break;
+        case 6:
+            cout<<"SATURDAY";
+            break;
+        case 7:
+            cout<<"SUNDAY";
+            break;
+        default:
+            cout<<"Invalid Day";
+    }
+}
