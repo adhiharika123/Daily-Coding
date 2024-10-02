dbdd8d6a56dbd3ab804e169640c55e1e9b4179c9
Adhi Harika <152500458+adhiharika123@users.noreply.github.com>
2024-10-02 17:13:35 +0530

    Add files via upload

---
+++ Loops.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //For loop
+    int i=1;
+    for(i;i<=10;i++){
+        cout<<"Hi I am "<<i<<endl;
+    }
+    //While Loop
+    while(i<=5){
+       cout<<"Hi I am while"<< i<<endl;
+       i=i+1;
+    }
+    //Do While
+    do{
+        cout<<"Hi I am do while"<< i<<endl;
+        i++;
+    }while(i<=1);
+}
