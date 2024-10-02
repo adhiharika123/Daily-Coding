2c480634dd9a5cba8b3ef781e0e567146ff3ce3c
Adhi Harika <152500458+adhiharika123@users.noreply.github.com>
2024-10-02 18:47:02 +0530

    Add files via upload

---
+++ PrintNameFunction.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printName( string name){
+    cout<<"The name is:"<<name;
+}
+int main(){
+    string name;
+    cout<<"Enter your name:";
+    cin>>name;
+    printName(name);
+    return 0;
+
+}
