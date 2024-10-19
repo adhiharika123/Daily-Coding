146a7db2433c9236d5b91f27fd9c4d30e9330e6f
Adhi Harika <152500458+adhiharika123@users.noreply.github.com>
2024-10-19 00:19:30 -0700

    Update Pattern5.cpp

--- Pattern5.cpp
+++ Pattern5.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 void print5(int n){
-    for(int i=1;i<=n;i++){
-        for(int j=0;j<n-i+1;j++){
+    for(int i=n;i>0;i--){
+        for(int j=i;j>0;j--){
             cout<<"* ";
         }
         cout<<endl;
