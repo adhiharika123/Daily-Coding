af70e98c5cdb8dd44eef197c251d0411cea8f7be
Adhi Harika <152500458+adhiharika123@users.noreply.github.com>
2024-11-05 08:41:19 -0800

    Update C++ STL-Stack.cpp

--- C++ STL-Stack.cpp
+++ C++ STL-Stack.cpp
@@ -27,6 +27,18 @@ void explainStack(){
     cout<<endl;
     //IS EMPTY OR NOT
     cout<<"Is Stack Empty or Not: "<<st.empty()<<endl;
+    //SWAP
+    stack<int> st1,st2;
+    //PUSH TO SWAP TO st2
+    st1.push(10);
+    st1.push(20);
+    st1.swap(st2);
+    cout << "st2 after swap: ";
+    while (!st2.empty()) {
+        cout << st2.top() << " ";
+        st2.pop();
+    }
+    cout << endl;
 }
 
 int main(){
@@ -41,5 +53,6 @@ The size of Stack is: 4
 Is Stack Empty or Not: 0
 The Elements in Stack are: 4 3 2 1
 Is Stack Empty or Not: 1
+st2 after swap: 20 10
 
-Process returned 0 (0x0)   execution time : 0.194 s
+Process returned 0 (0x0)   execution time : 0.138 s
